5fdeda8297322523eb974da45fdb79bc897a3a52
Sam-Kikoma <kikomasamson@gmail.com>
2024-05-04 19:23:39 +0200

    Component that displays user input

--- src/Components/Guess.jsx
+++ src/Components/Guess.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { range } from "../utils";
+import { checkGuess } from "../gameHelper";
 
-const Guess = ({ guess }) => {
+function Cell({ letter, status }) {
+	const className = status ? `cell ${status}` : `cell`;
+	return <span className={className}>{letter}</span>;
+}
+
+const Guess = ({ guess, answer }) => {
+	const result = checkGuess(guess, answer);
+	console.log(result);
 	return (
 		<>
-			<p className>
+			<p className="guess">
 				{range(5).map((num) => (
-					<span key={num}>{guess ? guess[num] : undefined}</span>
+					<Cell
+						key={num}
+						letter={result ? result[num].letter : undefined}
+						status={result ? result[num].status : undefined}
+					/>
 				))}
 			</p>
 		</>
