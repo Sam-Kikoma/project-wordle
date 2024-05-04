e165fc556b0ab6ecae65c581583255f0a6231799
Sam-Kikoma <kikomasamson@gmail.com>
2024-05-04 19:23:11 +0200

    Output component that renders row of Guess components

--- src/Components/Output.jsx
+++ src/Components/Output.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Guess from "./Guess";
 import { range } from "../utils";
+import { answerGen, answers } from "../answers";
 
 const Output = ({ guesses }) => {
+	const [answer, setAnswer] = useState(answerGen(answers));
+
+	console.log(answer);
 	return (
 		<>
 			{range(6).map((num) => (
-				<Guess key={num} guess={guesses[num]} />
+				<Guess key={num} guess={guesses[num]} answer={answer} />
 			))}
 		</>
 	);
